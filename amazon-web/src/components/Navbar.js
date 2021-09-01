f82b6ddaed939bd35c9fa291cf266cece42bd0dc
DileepKothapalli <65917448+DileepKothapalli@users.noreply.github.com>
2021-09-01 19:56:43 +0530

    nav

---
+++ amazon-web/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="left__navbar">
+        <h1>amazon.in</h1>
+        <h1>deliver to address</h1>
+      </div>
+      <div className="search__bar">search_bar</div>
+      <div className="right__navbar">right__navbar</div>
+    </div>
+  );
+};
+
+export default Navbar;
